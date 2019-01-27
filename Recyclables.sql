c6790de727e55b9d51efbc9608f3d5d1b7534aaf
Maxarreaga <34686490+Maxarreaga@users.noreply.github.com>
2019-01-27 09:03:53 -0500

    pictures + SQL additions

---
+++ Recyclables.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Naya water 600ml','057379600119','1','naya_water_500ml.jpg','naya');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Eska water 500ml','671785501008','1','eska_water_500ml.jpg','eska');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Rice Krispies Square','064100389014','0','rice_krispie.jpg','rice');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Lays Stax 155g','060410066294','1','lays_stax_sco.jpg','lays');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Lays Chip Bag','06041002560','0','lays_chip_bag_22g.jpg','lays');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('CLIF bar','722252120083','1','CLIF_bar_cpb.jpg','CLIF');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Coca-cola can 355ml','5449000000996','1','coke_can.jpg','coke');
+INSERT INTO items (name,barcode_num,recyclable,picture,keyword) VALUES ('Awake Chocolate 13.5g','891325004152','1','awake_bite.png','awake');
